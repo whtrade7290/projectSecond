ec7236bd4e7ad3d3f4aabccde4c4d36d71e89b4c
JeongWooHyeon <dngus7290@naver.com>
2021-07-27 22:53:15 +0900

    mybatis-config.xml 설정파일 안만들어서 에러 디펜던시 익셉션 에러 해결

--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -1,14 +1,10 @@
 package com.example.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.example.domain.TestVo;
 import com.example.service.TestService;
 
 import lombok.extern.java.Log;
@@ -20,11 +16,17 @@ public class HomeController {
 	@Autowired
 	TestService testService;
 	
-	@RequestMapping("/")
+	@RequestMapping(value = "/", method = RequestMethod.POST)
 	public String home() {
 		log.info("home() 호출됨");
 		return "home";
 	}
+	
+//	@RequestMapping("/")
+//	public String home() {
+//		log.info("home() 호출됨");
+//		return "home";
+//	}
 	
 
 
