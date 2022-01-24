06f536050f8f902be1f198f862ac94a611dfa210
JeongWooHyeon <dngus7290@naver.com>
2022-01-24 20:59:21 +0900

    게스트하우스 홈페이지 로그인 회원가입

--- src/main/java/com/example/SecondWebProjectApplication.java
+++ src/main/java/com/example/SecondWebProjectApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-
+//
 @SpringBootApplication
 @MapperScan("com.example.mapper")
 public class SecondWebProjectApplication {
